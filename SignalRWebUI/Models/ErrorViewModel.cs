10601af564e091e50d4900bbe02fc2b4b22bcf1b
Yusuf Bilgin <93447964+yusuf-bilgin@users.noreply.github.com>
2025-08-21 23:16:21 +0300

    Delete SignalRWebUI directory

--- SignalRWebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SignalRWebUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
