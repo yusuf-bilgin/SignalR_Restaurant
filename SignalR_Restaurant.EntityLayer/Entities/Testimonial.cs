65c0a6f331a84419f99c33fa3a92a563f0c3442e
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-08-23 12:25:17 +0300

    feat(core): add Contact, SocialMedia, and Testimonial entities; implement AddTestimonial feature

--- SignalR_Restaurant.EntityLayer/Entities/Testimonial.cs
+++ SignalR_Restaurant.EntityLayer/Entities/Testimonial.cs
@@ -1,6 +1,14 @@
-﻿namespace SignalR_Restaurant.EntityLayer.Entities
+﻿using SignalR_Restaurant.EntityLayer.Enums;
+
+namespace SignalR_Restaurant.EntityLayer.Entities
 {
     public class Testimonial
     {
+        public int TestiomonialId { get; set; }
+        public string? Name { get; set; }
+        public string? Title { get; set; }
+        public string? Comment { get; set; }
+        public string? ImageUrl { get; set; }
+        public StatusType Status { get; set; }
     }
 }
