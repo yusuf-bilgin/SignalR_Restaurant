82af2bc691bb85d47532eda0207f11c25c7dee79
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-08-25 17:15:29 +0300

    feat(DataAccessLayer): add GenericRepository<T> implementation
    Added GenericRepository<T> to centralize basic CRUD operations via RestaurantContext.
    This enables dependency injection of data-access components and reduces code duplication.

---
+++ SignalR_Restaurant.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR_Restaurant.DataAccessLayer.Abstract;
+using SignalR_Restaurant.DataAccessLayer.Concrete;
+
+namespace SignalR_Restaurant.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly RestaurantContext _context;
+
+        public GenericRepository(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
