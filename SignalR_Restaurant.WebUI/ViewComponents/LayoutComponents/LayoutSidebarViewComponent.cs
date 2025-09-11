39151de70383f93e67819e7d1e60ed6960a8a746
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-09-11 15:46:00 +0300

    refactor(ui): modularize layout into reusable view components
    - Added LayoutFooter, LayoutNavbar, LayoutScript, and LayoutSidebar view components
    - Moved related HTML into Shared/Components views
    - Updated layout to use @await Component.InvokeAsync for rendering
    - Fixed script execution issues by isolating scripts in LayoutScript component

---
+++ SignalR_Restaurant.WebUI/ViewComponents/LayoutComponents/LayoutSidebarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_Restaurant.WebUI.ViewComponents.LayoutComponents
+{
+    public class LayoutSidebarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
