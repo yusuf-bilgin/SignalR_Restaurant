277821926d3de1d2df8c14fb2f69e9b74045aa49
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-10-21 12:27:27 +0300

    feat(client): add ClientLayoutController and base layout view for client-side pages
    - Created ClientLayoutController for managing client layout structure
    - Added initial client layout view (no layout used)
    - Copied HTML template and pasted into layout index view to be modularized into ViewComponents later

---
+++ SignalR_Restaurant.WebUI/Controllers/Client/ClientLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_Restaurant.WebUI.Controllers.Client
+{
+    public class ClientLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
