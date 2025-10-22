6bf7e0454659f107cddc48fa10e10bf8c06bdaa4
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-10-22 18:50:07 +0300

    feat(client): add additional home section view components
    - Added ClientHomeAboutViewComponent, ClientHomeBookViewComponent, ClientHomeMenuViewComponent, and ClientHomeTestimonialViewComponent
    - Created corresponding Default.cshtml files under Shared/Components
    - Updated ClientLayout/Index.cshtml and Default/Index.cshtml to render new components
    - Improved modular structure for client-side layout and home sections

---
+++ SignalR_Restaurant.WebUI/ViewComponents/ClientLayoutComponents/ClientHomeComponents/ClientHomeAboutViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_Restaurant.WebUI.ViewComponents.ClientLayoutComponents.ClientHomeComponents
+{
+    public class ClientHomeAboutViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalR_Restaurant.WebUI/ViewComponents/ClientLayoutComponents/ClientHomeComponents/ClientHomeBookViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_Restaurant.WebUI.ViewComponents.ClientLayoutComponents.ClientHomeComponents
+{
+    public class ClientHomeBookViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
