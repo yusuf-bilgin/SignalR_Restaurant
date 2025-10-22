61ff776820d1d4cad314be464ed5b3e0b5c71e0a
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-10-22 16:31:58 +0300

    feat(client): add home offer and slider view components
    - Added ClientHomeOfferViewComponent and ClientHomeSliderViewComponent
    - Created corresponding Default.cshtml views under Shared/Components
    - Updated ClientLayout/Index.cshtml to include new components

---
+++ SignalR_Restaurant.WebUI/ViewComponents/ClientLayoutComponents/ClientHomeComponents/ClientHomeSliderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_Restaurant.WebUI.ViewComponents.ClientLayoutComponents.ClientHomeComponents
+{
+    public class ClientHomeSliderViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
