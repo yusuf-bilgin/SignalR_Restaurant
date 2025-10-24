0889b51d6b28e5c652ea1c968e1e427859b8a63d
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-10-24 23:30:03 +0300

    feat(client): integrate product API and display featured products on homepage
    - Added ClientHomeMenuViewComponent to consume product data from API
    - Implemented async data fetching via IHttpClientFactory
    - Deserialized product list using ResultProductDto
    - Updated ClientHomeMenu view to dynamically render products with name, description, image, and price
    - Added navigation links to menu page

--- SignalR_Restaurant.WebUI/ViewComponents/ClientLayoutComponents/ClientHomeComponents/ClientHomeMenuViewComponent.cs
+++ SignalR_Restaurant.WebUI/ViewComponents/ClientLayoutComponents/ClientHomeComponents/ClientHomeMenuViewComponent.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR_Restaurant.WebUI.Dtos.ProductDtos;
 
 namespace SignalR_Restaurant.WebUI.ViewComponents.ClientLayoutComponents.ClientHomeComponents
 {
     public class ClientHomeMenuViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ClientHomeMenuViewComponent(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7181/api/Product");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            return View(values);
         }
     }
 }
