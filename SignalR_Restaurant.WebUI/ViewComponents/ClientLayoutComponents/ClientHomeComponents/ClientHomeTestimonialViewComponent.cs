8ab663c700d804086d0c03762ffbe6131a78f4c3
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-10-24 23:55:23 +0300

    feat(client): consume Testimonial API and render dynamic Testimonial section

--- SignalR_Restaurant.WebUI/ViewComponents/ClientLayoutComponents/ClientHomeComponents/ClientHomeTestimonialViewComponent.cs
+++ SignalR_Restaurant.WebUI/ViewComponents/ClientLayoutComponents/ClientHomeComponents/ClientHomeTestimonialViewComponent.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR_Restaurant.WebUI.Dtos.TestimonialDtos;
 
 namespace SignalR_Restaurant.WebUI.ViewComponents.ClientLayoutComponents.ClientHomeComponents
 {
     public class ClientHomeTestimonialViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ClientHomeTestimonialViewComponent(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7181/api/Testimonial");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+            var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+            return View(values);
         }
     }
 }
