225ae0834d4b5aeeb824fbc7eb39716f4ff1b546
yusuf-bilgin <yusufbilgin1905@gmail.com>
2025-10-27 13:45:57 +0300

    ⚠️feat(client): add menu page layout and integrate product data fetching
    - Implemented ClientMenuController and view structure for the menu page
    - Added ClientMenuViewComponent to dynamically fetch and display product data
    - Menu page design finalized and integrated with existing layout
    - **Note:** Menu page has its own header (duplicate with layout header)
    - **Note:** Product category filtering is under development (not yet implemented)

---
+++ SignalR_Restaurant.WebUI/Controllers/Client/ClientMenuController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR_Restaurant.WebUI.Dtos.ProductDtos;
+
+namespace SignalR_Restaurant.WebUI.Controllers.Client
+{
+    public class ClientMenuController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ClientMenuController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7181/api/Product");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            return View(values);
+        }
+    }
+}
---
+++ SignalR_Restaurant.WebUI/ViewComponents/ClientLayoutComponents/ClientMenuComponents/ClientMenuNavbarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR_Restaurant.WebUI.ViewComponents.ClientLayoutComponents.ClientMenuComponents
+{
+    public class ClientMenuNavbarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
